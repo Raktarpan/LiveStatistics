d0576305a8a5504f73848b9b08c2e6bcdcc27e32
Hrishikesh <hrishirt@cse.iitk.ac.in>
2017-06-10 16:02:22 +0530

    basic structure

---
+++ live.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('config.php');
+$CampId = "1";
+
+?>
+<html>
+<head>
+<title>&bull; Raktarpan &bull; Blood Donation Camp &bull; Live Statistics &bull;</title>
+</head>
+<body>
+    <div id="content">
+    <div id="total"> 
+    <?php ?>
+    </div>
+    <div id="stat-bldgrp"> 
+    
+    </div>
+    <div id="stat-year"> 
+    
+    </div>
+    <div id="stat-hall"> 
+    
+    </div>
+    </div>
+</body>
+</html>
